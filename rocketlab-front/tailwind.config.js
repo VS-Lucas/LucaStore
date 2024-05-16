4cbe9ad3f6d5706b4b4b0c69db67d61e52b2ae18
VS-Lucas <ufpelucasv@gmail.com>
2024-05-16 09:11:48 -0300

    feat: flowbite

--- rocketlab-front/tailwind.config.js
+++ rocketlab-front/tailwind.config.js
@@ -1,12 +1,14 @@
+import flowbite from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [flowbite],
+};
